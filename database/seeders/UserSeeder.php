89836bf14b39786099d092e4375b554b1b1c783c
zaenalrfn <ZAOfficial24@gmail.com>
2024-12-26 10:11:24 +0700

    done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,8 +15,8 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name' => 'banjar',
-            'email' => 'rifaldobanjar@gmail.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('12345'),
         ]);
     }
